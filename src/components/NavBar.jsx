d435331bbe13b0a03643cc2fb35f46a7813c038f
José Cordeiro <zecaxcr@gmail.com>
2022-12-08 18:54:15 -0300

    navbar is done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
-import { Search } from "@material-ui/icons";
 import React from "react";
+
 import styled from "styled-components";
+
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 
 const Container = styled.div`
   height: 60px;
@@ -9,6 +12,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
@@ -21,19 +25,40 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
-`
+`;
 
 const SearchContainer = styled.div`
-  border: 1px solid lightgray;
+  border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
 
-`
+const Input = styled.input`
+  border: none;
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const NavBar = () => {
@@ -43,12 +68,22 @@ const NavBar = () => {
         <Left>
           <Language>PT</Language>
           <SearchContainer>
-              input
-              <Search/>
+            <Input></Input>
+            <Search style={{color: "gray", fontSize: 16}} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>ZK CLOTHING.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Criar conta</MenuItem>
+          <MenuItem>Entrar</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined/>
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
