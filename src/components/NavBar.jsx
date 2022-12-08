f8a34dd061e9b7e4f6aadf4939a10e323cd338d4
José Cordeiro <zecaxcr@gmail.com>
2022-12-08 18:37:46 -0300

    editing left side

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -11,15 +12,41 @@ const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+
+`
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const NavBar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>Left</Left>
+        <Left>
+          <Language>PT</Language>
+          <SearchContainer>
+              input
+              <Search/>
+          </SearchContainer>
+        </Left>
         <Center>Center</Center>
         <Right>Right</Right>
       </Wrapper>
