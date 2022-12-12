1e9361c0b47ad0298bb520fd427204ebabaa3f1e
José Cordeiro <zecaxcr@gmail.com>
2022-12-12 14:56:19 -0300

    update slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,34 +6,76 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  background-color: coral;
 `;
 
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    background-color: #fff3f3;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: ${props => props.direction === "left" && "10px"};
-    right: ${props => props.direction === "right" && "10px"};
-    margin: auto;
-`
+  width: 50px;
+  height: 50px;
+  background-color: #fff3f3;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+    height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1``
+const Description = styled.p``
+const Button = styled.button``
 
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
-        <ArrowLeft/>
+        <ArrowLeft />
       </Arrow>
+      <Wrapper>
+        <Slide></Slide>
+        <ImgContainer>
+          <Image src="" />
+        </ImgContainer>
+        <InfoContainer>
+            <Title></Title>
+            <Description></Description>
+            <Button></Button>
+        </InfoContainer>
+      </Wrapper>
       <Arrow direction="right">
-        <ArrowRight/>
+        <ArrowRight />
       </Arrow>
     </Container>
   );
