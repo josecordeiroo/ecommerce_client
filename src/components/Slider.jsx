45161cc135773d37d29e0b9fe7d1bd18544fa6dc
José Cordeiro <zecaxcr@gmail.com>
2022-12-12 14:44:09 -0300

    create arrows

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
+import { ArrowLeft, ArrowRight } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: coral;
+  position: relative;
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff3f3;
+    border-radius: 50%;
     display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
 `
 
 const Slider = () => {
   return (
-    <Container><Arrow></Arrow></Container>
-  )
-}
-import styled from 'styled-components'
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeft/>
+      </Arrow>
+      <Arrow direction="right">
+        <ArrowRight/>
+      </Arrow>
+    </Container>
+  );
+};
 
-export default Slider
+export default Slider;
