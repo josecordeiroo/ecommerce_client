946273258a3302b82532225d343a0d83578ac398
José Cordeiro <zecaxcr@gmail.com>
2022-12-12 18:16:32 -0300

    edit data

--- src/data.js
+++ src/data.js
@@ -1,1 +1,23 @@
-export const sliderItems
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/s3xjxFz/kisspng-woman-female-portable-network-graphics-clip-art-fa-zenle-seilmi-png-bayan-resimleri-womens-5.png",
+    title: "MODA VERÃO",
+    desc: "NAO PERCA A OPORTUNIDADE",
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/s3xjxFz/kisspng-woman-female-portable-network-graphics-clip-art-fa-zenle-seilmi-png-bayan-resimleri-womens-5.png",
+    title: "MODA VERÃO",
+    desc: "NAO PERCA A OPORTUNIDADE",
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/s3xjxFz/kisspng-woman-female-portable-network-graphics-clip-art-fa-zenle-seilmi-png-bayan-resimleri-womens-5.png",
+    title: "MODA VERÃO",
+    desc: "NAO PERCA A OPORTUNIDADE",
+    bg: "f5fafd",
+  },
+];
