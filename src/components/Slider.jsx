be3d8d96c86c6fed2bacb6080fd6df988677aa23
José Cordeiro <zecaxcr@gmail.com>
2022-12-12 14:28:38 -0300

    create slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+
+const Slider = () => {
+  return (
+    <Container><Arrow></Arrow></Container>
+  )
+}
+import styled from 'styled-components'
+
+export default Slider
