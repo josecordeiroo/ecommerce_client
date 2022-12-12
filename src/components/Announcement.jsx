040b01a0576c914b1ae75297845d1703f3683467
José Cordeiro <zecaxcr@gmail.com>
2022-12-12 14:26:45 -0300

    announcement is done

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    height: 30px;
-    background-color: teal;
-    color: white;
-`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
 
 const Announcement = () => {
   return (
-    <Container>Super promoção! Frete grátis em compras acima de R$50.</Container>
-  )
-}
+    <Container>
+      Super promoção! Frete grátis em compras acima de R$50.
+    </Container>
+  );
+};
 
-export default Announcement
+export default Announcement;
