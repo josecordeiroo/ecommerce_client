c9b587ffc776de8a2ad9bfc995bd948e3d491495
José Cordeiro <zecaxcr@gmail.com>
2022-12-24 18:39:53 -0300

    create filter by color

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,42 +1,70 @@
 import React from "react";
 import styled from "styled-components";
 
-import NavBar from "../components/NavBar"
-import Announcement from "../components/Announcement"
-import Products from "../components/Products"
-import Newsletter from "../components/Newsletter"
-import Footer from "../components/Footer"
+import NavBar from "../components/NavBar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Container = styled.div``;
 
 const Title = styled.h1`
-    margin: 20px;
+  margin: 20px;
 `;
 
 const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const Filter = styled.div``;
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Select = styled.select`
+
+`
+
+const Option = styled.option`
+
+`
 
 const ProductList = () => {
-  return <Container>
-    <Announcement/>
-    <NavBar/>
-    <Title>Testando</Title>
-    <FilterContainer>
+  return (
+    <Container>
+      <Announcement />
+      <NavBar />
+      <Title>Camisetas</Title>
+      <FilterContainer>
         <Filter>
-            Filtro 1
+          <FilterText>Filtrar produtos:</FilterText>
+          <Select>
+            <Option disabled selected>
+                Cor
+            </Option>
+            <Option>Branco</Option>
+            <Option>Preto</Option>
+            <Option>Vermelho</Option>
+            <Option>Azul</Option>
+            <Option>Amarelo</Option>
+            <Option>Verde</Option>
+          </Select>
         </Filter>
         <Filter>
-            Filtro 2
+          <FilterText>Classificar produtos:</FilterText>
         </Filter>
-    </FilterContainer>
-    <Products/>
-    <Newsletter/>
-    <Footer/>
-  </Container>;
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
 };
 
 export default ProductList;
