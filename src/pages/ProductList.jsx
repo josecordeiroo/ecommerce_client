9d394864e05b0e786888264d71f5e39da4f02dc7
José Cordeiro <zecaxcr@gmail.com>
2022-12-24 18:27:13 -0300

    import navbar

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+import NavBar from "../components/NavBar"
+
 const Container = styled.div``;
 
 const ProductList = () => {
-  return <Container></Container>;
+  return <Container>
+    <NavBar/>
+  </Container>;
 };
 
 export default ProductList;
