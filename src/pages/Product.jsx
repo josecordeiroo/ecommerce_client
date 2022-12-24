4eeb3f21451ad4bc058c51602446284747f06860
José Cordeiro <zecaxcr@gmail.com>
2022-12-24 19:56:02 -0300

    product page is done

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,7 +5,7 @@ import NavBar from "../components/NavBar";
 import Announcement from "../components/Announcement";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
-import { Filter } from "@material-ui/icons";
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div``;
 
@@ -13,28 +13,101 @@ const Wrapper = styled.div`
   padding: 50px;
   display: flex;
 `;
+
 const ImgContainer = styled.div`
   flex: 1;
 `;
 
 const Image = styled.img`
-  margin-top: -60px;
-  height: 90vh;
+  width: 100%;
   object-fit: cover;
 `;
+
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
 `;
+
 const Title = styled.h1`
   font-weight: 200;
 `;
+
 const Desc = styled.p`
   margin: 20px 0px;
 `;
+
 const Price = styled.span`
   font-weight: 100;
   font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-style: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 1px 0px 10px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-content: center;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4 ;
+    }
 `;
 
 const Product = () => {
@@ -44,23 +117,23 @@ const Product = () => {
       <NavBar />
       <Wrapper>
         <ImgContainer>
-          <Image src="https://d3ugyf2ht6aenh.cloudfront.net/stores/119/911/products/camiseta-zoombies-from-space-frente1-b08378aae324a563c416333529530407-640-0.webp" />
+          <Image src="https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png" />
         </ImgContainer>
         <InfoContainer>
-          <Title>Camiseta - Zoombies Space</Title>
+          <Title>Camiseta - Life is Good</Title>
           <Desc>
             Estampa em Silk na parte frontal. Detalhe em silk nas costas.
             Confeccionada com linha 100% poliéster (maior durabilidade na
             costura). Com reforço de ombro a ombro com viés (a cor do viés pode
             alterar de acordo com as reposições do produto). Composição: Malha
-            100% Algodão, fio 30.1 penteada. Cor predominante: Preto.
+            100% Algodão, fio 30.1 penteada. Cor predominante: Antique White.
           </Desc>
           <Price>R$ 99.90</Price>
           <FilterContainer>
             <Filter>
               <FilterTitle>Cor</FilterTitle>
-              <FilterColor color="black" />
-              <FilterColor color="darkblue" />
+              <FilterColor color="antiquewhite" />
+              <FilterColor color="aquamarine" />
               <FilterColor color="gray" />
             </Filter>
             <Filter>
@@ -75,6 +148,14 @@ const Product = () => {
               </FilterSize>
             </Filter>
           </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+                <Remove/>
+                <Amount>1</Amount>
+                <Add/>
+            </AmountContainer>
+            <Button>ADICIONAR AO CARRINHO</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
