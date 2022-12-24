559d734abb8bd3317e6a764b613452b993af0b0c
José Cordeiro <zecaxcr@gmail.com>
2022-12-23 21:54:31 -0300

    change products style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,14 +8,53 @@ import {
 } from "@material-ui/icons";
 
 const Container = styled.div`
-    flex: 1;
-    margin: 5px;
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 `;
 
-const Circle = styled.div``
-const Image = styled.img``
-const Info = styled.div``
-const Icon = styled.div``
+const Circle = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Info = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0%;
+    background-color: gray;
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+`;
 
 export const Product = ({ item }) => {
   return (
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,6 +7,8 @@ import Product from "./Product";
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 export const Products = () => {
