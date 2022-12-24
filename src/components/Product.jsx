3736139894d386065cf1745cf455c37206ef2d92
José Cordeiro <zecaxcr@gmail.com>
2022-12-23 22:00:55 -0300

    add transition to product container

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,6 +7,21 @@ import {
   ShoppingCartOutlined,
 } from "@material-ui/icons";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0%;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -17,14 +32,18 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    background-color: white;
-    position: absolute;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
 `;
 
 const Image = styled.img`
@@ -32,28 +51,22 @@ const Image = styled.img`
   z-index: 2;
 `;
 
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0%;
-    background-color: gray;
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
 const Icon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `;
 
 export const Product = ({ item }) => {
