eada3f0cc2564d0c175370eafc7703cabac799d5
José Cordeiro <zecaxcr@gmail.com>
2022-12-24 19:27:36 -0300

    add all components of page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,13 +7,34 @@ import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.span``;
-const Price = styled.span``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  margin-top: -60px;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+const Title = styled.h1`
+    font-weight: 200;
+`;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
 
 const Product = () => {
   return (
