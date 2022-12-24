3b7ea19e880e7714d86714857ea067dc8643ada6
José Cordeiro <zecaxcr@gmail.com>
2022-12-24 18:33:02 -0300

    import components

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,13 +3,39 @@ import styled from "styled-components";
 
 import NavBar from "../components/NavBar"
 import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 const Container = styled.div``;
+
+const Title = styled.h1`
+    margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div``;
 
 const ProductList = () => {
   return <Container>
     <Announcement/>
     <NavBar/>
+    <Title>Testando</Title>
+    <FilterContainer>
+        <Filter>
+            Filtro 1
+        </Filter>
+        <Filter>
+            Filtro 2
+        </Filter>
+    </FilterContainer>
+    <Products/>
+    <Newsletter/>
+    <Footer/>
   </Container>;
 };
 
