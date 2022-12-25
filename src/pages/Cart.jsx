30b20813453eaec37a8491581dba5c4f7025013a
José Cordeiro <zecaxcr@gmail.com>
2022-12-25 10:24:13 -0300

    create new page Cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Cart = () => {
+  return (
+    <Container>Hellow</Container>
+  )
+}
+
+export default Cart
