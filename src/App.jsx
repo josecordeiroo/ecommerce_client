c8b26e6c406ca5173365946bbc178e0baa85c8d3
José Cordeiro <zecaxcr@gmail.com>
2023-02-08 12:41:01 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import Cart from "./pages/Cart";
+import React from "react";
+
+//Routes
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <Register/>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
