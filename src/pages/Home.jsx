fae34d9423ead715852e36ce736b42e5a32cbe52
José Cordeiro <zecaxcr@gmail.com>
2023-02-19 12:52:06 -0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
+//update
+
 const Home = () => {
   return (
     <div>
