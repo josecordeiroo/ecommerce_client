f5408fc61559426f7c27643f90995c4220eaef30
José Cordeiro <zecaxcr@gmail.com>
2023-03-08 10:07:54 -0300

    update redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,37 +3,38 @@ import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
 import {
-  persistreStore,
+  persistStore,
   persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
-} from 'redux-persist'
+  REGISTER,
+} from "redux-persist";
 
-import storage from 'redux-persist/lib/storage'
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   version: 1,
-  storage
-}
+  storage,
+};
 
-const persistedReducer = persistReducer(persistConfig, userReducer)
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
-export const store = () => configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: persistReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    })
-});
+export const store = () =>
+  configureStore({
+    reducer: {
+      cart: cartReducer,
+      user: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  });
 
-export let persistor = persistStore(store)
+export let persistor = persistStore(store);
