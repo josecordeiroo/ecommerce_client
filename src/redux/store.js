510611cf4a8efd28f5c993dacadafdd15f3eff3c
José Cordeiro <zecaxcr@gmail.com>
2023-03-08 10:05:22 -0300

    import redux-persist to project

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,38 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
-export default configureStore({
+import {
+  persistreStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export const store = () => configureStore({
   reducer: {
     cart: cartReducer,
-    user: userReducer,
+    user: persistReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+export let persistor = persistStore(store)
