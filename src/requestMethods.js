9a0af8d16f93e74dc37ca25ae686222a6a15b664
José Cordeiro <zecaxcr@gmail.com>
2023-03-13 18:57:32 -0300

    update in request methods token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGQxNzRkZTVmNDdmNzc4YzU3YzgwYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NjI5MjY4OSwiZXhwIjoxNjc2NTUxODg5fQ.L8qFMs_pQYKT5YHBzxYiKXOVRXoIEcV6_ZBOojo73Yc";
+const TOKEN = JSON.parse(
+  JSON.parse(localStorage.getItem("persist:root")).currentUser
+).accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
