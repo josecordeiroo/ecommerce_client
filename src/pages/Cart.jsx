d1738388fecdac0e14a7c15c00a64af9cbd5aca8
José Cordeiro <zecaxcr@gmail.com>
2023-08-19 12:47:21 -0300

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -138,7 +138,7 @@ const Summary = styled.div`
   border: 0.5px solid lightgray;
   border-radius: 10px;
   padding: 20px;
-  height: 50vh; //update
+  height: 50vh;
 `;
 
 const SummaryTitle = styled.h1`
